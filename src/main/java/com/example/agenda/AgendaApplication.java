a98d168b20825be95517e52a285a9f79766a03a3
Baljaudev <r.baljau@gmail.com>
2022-10-27 13:42:34 +0200

    Initial commit. Uploading project

---
+++ src/main/java/com/example/agenda/AgendaApplication.java
@@ -0,0 +1,73 @@
+package com.example.agenda;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class AgendaApplication extends Application {
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("AddressApp");
+
+        initRootLayout();
+
+        showPersonOverview();
+    }
+
+    /**
+     * Inicializa el layout principal de la aplicación.
+     */
+    public void initRootLayout() {
+        try {
+            // Cargamos el layout raíz desde el fichero fxml:
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(AgendaApplication.class.getResource("RootLayout.fxml"));
+            rootLayout = loader.load();
+
+            //Muestra la escena que contiene el layout raíz:
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Muestra el fichero FXML dentro del layout raíz.
+     */
+    public void showPersonOverview() {
+        try {
+            // Carga el fichero fxml que le pasemos por parámetro. Debemos referirnos al Pane que tiene:
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(AgendaApplication.class.getResource("PersonOverview.fxml"));
+            AnchorPane personOverview = loader.load();
+
+            //Establece el fxml de PersonOverview en el centro del layout raíz:
+            rootLayout.setCenter(personOverview);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Returns the main stage.
+     * @return
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
